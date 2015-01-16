53a4a4edcb2efe646b6f1fd1f5ae960156359f58
Vincy Cyrill <vincy@bpatech.com>
2015-01-16 14:12:18 +0530

    Added new output line in sample.java

--- HelloWorld/src/com/bpa/test/Sample.java
+++ HelloWorld/src/com/bpa/test/Sample.java
@@ -3,6 +3,7 @@ package com.bpa.test;
 public class Sample {
 	public static void main(String[] args){
 		System.out.println("welcome");
-		//System.out.println("test1");
+		System.out.println("test1");
 	}
 }
+/*Welcome*/
